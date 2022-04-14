2c0ec08ff8d16c6231fa509b18b24d05dee0205b
mosheurpau <mosheur.pau@gamil.com>
2022-04-15 00:08:36 +0600

    Added auth redirect and signout

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,15 +1,16 @@
-import React, { Children } from "react";
+import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../../firebase.init";
 
-const RequireAuth = () => {
+const RequireAuth = ({ children }) => {
   const [user] = useAuthState(auth);
   const location = useLocation();
+  console.log(user);
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
-  return Children;
+  return children;
 };
 
 export default RequireAuth;
